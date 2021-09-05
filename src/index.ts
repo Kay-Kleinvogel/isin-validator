2f91720761624a6ca8d069a417c76f5d22bbb758
Kay Kleinvogel <kaykleinvogel@googlemail.com>
2021-09-05 14:02:08 +0200

    working implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,71 @@
-export const sum = (a: number, b: number): number => {
-	const addition = a + b;
+const DEBUG_MODE = false;
 
-  return addition;
-};
+export const isValidIsin = (isin: string): boolean => {
+  if (!isin) {
+	if(DEBUG_MODE) console.log("isin is empty");
+    return false;
+  }
+  if(isIsinFormat(isin)){
+	  if(DEBUG_MODE) console.log("isin is valid");
+	const numberIsin = translateNonNumericCharacters(isin);
+	return isValidChecksum(numberIsin);
+  }
+  if(DEBUG_MODE) console.log("isin is invalid");
+  return false;
+}
+
+// checks if the isin is in the correct format
+export const isIsinFormat = (isin: string): boolean => {
+  const isinRegex = /^[A-Z]{2}[A-Z0-9]{9}[0-9]$/;
+  return isinRegex.test(isin);
+}
+
+// removes country code from isin
+export const removeCountryCode = (isin: string): string => {
+  return isin.substr(2);
+}
+
+// translates non-numeric characters from isin
+export const translateNonNumericCharacters = (isin: string): string => {
+  const charArray = isin.toUpperCase().split('');
+  const numericArray = charArray.map(char => {
+    if (!isANumber(char)) {
+      return parseInt(char, 36);
+    }
+    return Number(char);
+  });
+  if (DEBUG_MODE) {
+	console.log(isin)
+    	console.log(numericArray);
+  }
+  return numericArray.join('');
+}
+
+export const isANumber = (char: string): boolean => {
+  return !isNaN(Number(char));
+}
+
+export const isValidChecksum = (isin: string): boolean => {
+	const numberArray = isin.split('').map(char => Number(char));
+	const baseSum = numberArray.pop();
+	numberArray.reverse();
+	let sum = 0;
+	numberArray.forEach((number, index) => {
+		if(DEBUG_MODE) console.log(number, index % 2 === 0);
+		if (index % 2 === 0) {
+			if(number * 2 > 9) {
+				sum += (number * 2) - 9;
+			} else {
+				sum += number * 2;
+			}
+		} else {
+			sum += number;
+		}
+	});
+	const newSum = 10 - (sum % 10) % 10;
+	if(DEBUG_MODE){
+		console.log(baseSum);
+		console.log(newSum);
+	}
+	return newSum === baseSum;
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,41 @@
-import { sum } from "../src";
+import { removeCountryCode, translateNonNumericCharacters, isValidChecksum } from './../src/index';
+import { isValidIsin, isIsinFormat, isANumber } from "../src";
 
-describe("sum", () => {
-  it("should return sum of two numbers", () => {
-    expect(sum(1, 2)).toEqual(3);
+describe("isIsinFormat", () => {
+  it("should return whether the string is in isin format", () => {
+    const validIsin = "US0378331005";
+    expect(isIsinFormat(validIsin)).toEqual(true);
   });
+  it('should detect if the isin is correct', () => {
+    const validIsin1 = "DE000BAY0017";
+    const validIsing2 = "AU0000XVGZA3";
+    const invalidIsin = "DE000BAY0018";
+    expect(isValidIsin(validIsin1)).toEqual(true);
+    expect(isValidIsin(validIsing2)).toEqual(true);
+    expect(isValidIsin(invalidIsin)).toEqual(false);
+  })
 });
+
+describe ('isinFormater', () => {
+  it('should remove the country prefix of the isin', () => {
+    const isin = 'US0378331005';
+    const targetIsin = '0378331005';
+    expect(removeCountryCode(isin)).toEqual(targetIsin);
+  })
+  it('should translate non numeric characters to numeric characters', () => {
+    const nonNumericCharacter = 'DE000BAY001';
+    const expectedResult = "1314000111034001";
+    expect(translateNonNumericCharacters(nonNumericCharacter)).toEqual(expectedResult);
+  })
+});
+
+describe ('utilities', () => {
+  it('isANumbers should detect numbers', () => {
+    expect(isANumber('1')).toEqual(true);
+    expect(isANumber('A')).toEqual(false);
+  })
+  it('should detect a correct checksum', () => {
+    expect(isValidChecksum('13140001110340017')).toEqual(true);
+    expect(isValidChecksum('13140001110340018')).toEqual(false);
+  })
+})
